8561829e05a83f87936c3497eefbe8aaec0b5a3c
Jiayi Ren <jiayiren22@gmail.com>
2020-07-14 21:06:29 -0700

    add env for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config();
+
 const server = require('./server')
-const port = 8000
-server.listen(8000, () =>{
-    console.log('\n*** Server Running on http://localhost:8000 ***\n')
+const port = process.env.PORT || 8000;
+server.listen(port, () =>{
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
 })
